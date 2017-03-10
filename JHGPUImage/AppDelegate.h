8d8a3be50a2f5046679e77a3f3e4cc1fe40b1d03
jiajianhao <you@example.com>
2017-03-10 16:39:28 +0800

    Initial Commit

---
+++ JHGPUImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JHGPUImage
+//
+//  Created by admin on 2017/3/10.
+//  Copyright © 2017年 jiajianhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JHGPUImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JHGPUImage
+//
+//  Created by admin on 2017/3/10.
+//  Copyright © 2017年 jiajianhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
